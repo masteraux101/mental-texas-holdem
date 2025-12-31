92e665b2198b199e3806fa0392da45d0ec26b340
masteraux101 <768471488@qq.com>
2025-12-31 16:06:33 +0800

    fix: correct formatting of iceServers declaration in setup

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -8,13 +8,18 @@ import ChatRoom, { ChatRoomEvent } from "./ChatRoom";
 const iceServers = [
   {
     urls: "stun:stun.relay.metered.ca:80",
+  },
+  { 
+    urls: 'stun:stun.l.google.com:19302' 
   }
-
 ];
 
 const peer = new Peer(
   {
-    debug: 3
+    debug: 3,
+    config:{
+      iceServers: iceServers,
+    }
   } as PeerOptions
 );
 
